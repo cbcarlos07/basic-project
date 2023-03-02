58c9f55e44880a9b3bce5f7b0c5d412880c7141e
Carlos Bruno <carlosbrito@movida.com.br>
2023-03-02 18:22:59 -0300

    npdeṕrt

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const app = require('./app')
 
-const SERVER_PORT = process.env.SERVER_PORT
+const SERVER_PORT = 3000
 
 app.listen( SERVER_PORT, () => {
     console.log(`API rodando na porta ${SERVER_PORT}...`);
