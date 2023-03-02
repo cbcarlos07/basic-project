d1e502550a19d2b377fab6d7f0818a936a67b29c
Carlos Bruno <carlosbrito@movida.com.br>
2023-03-02 14:36:04 -0300

    ajuste

--- src/routes/initRoute.js
+++ src/routes/initRoute.js
@@ -3,7 +3,7 @@ const route = express.Router()
 const package = require('../../package.json')
 
 route.get('/', (req, res)=>{
-    res.json({msg: `Bem vindo à API - v${package.version}`})
+    res.json({msg: `Bem vindo à API - v${package.version}!`})
 })
 
 module.exports = route
