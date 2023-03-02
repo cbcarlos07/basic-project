23b46fcf90c7e31b1a9241d48b312e64d5c572d2
Carlos Bruno <carlosbrito@movida.com.br>
2023-03-02 15:22:06 -0300

    configMap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 const app = require('./app')
 
 const SERVER_PORT = process.env.SERVER_PORT
+
 console.log('env',process.env);
+
 app.listen( SERVER_PORT, () => {
     console.log(`API rodando na porta ${SERVER_PORT}...`);
 } )
