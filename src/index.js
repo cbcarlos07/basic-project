94676de7d01cb587185358df7bdcd93237b870b8
Carlos Bruno <carlosbrito@movida.com.br>
2023-03-02 15:21:22 -0300

    ajuste

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const app = require('./app')
 
 const SERVER_PORT = process.env.SERVER_PORT
-
+console.log('env',process.env);
 app.listen( SERVER_PORT, () => {
     console.log(`API rodando na porta ${SERVER_PORT}...`);
 } )
