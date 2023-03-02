c7d19b64f5eeb4ff92a5413ad524bcad843ce65c
Carlos Bruno <carlosbrito@movida.com.br>
2023-03-02 17:25:58 -0300

    porta

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ const app = require('./app')
 
 const SERVER_PORT = process.env.SERVER_PORT
 
-console.log('env',process.env);
-
 app.listen( SERVER_PORT, () => {
     console.log(`API rodando na porta ${SERVER_PORT}...`);
 } )
